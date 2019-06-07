918ac674640c497b84a280858a133a445db5ac4f
yagmurcetintas <yagmurcetin@gmail.com>
2019-06-07 09:11:28 -0400

    added game logic and dynamic html

--- assets/game.js
+++ assets/game.js
@@ -4,63 +4,63 @@ var counter = 30;
 var intervalId;
 var questions = [{
         question: "What is the number of the vault that belongs to Harry at the Gringotts Wizarding Bank?",
-        answer: ["394", "713", "547"],
+        answer: ["394", "687", "713", "547"],
         correctAnswer: "687",
-        image: "",
+        image: "gringotts.jpg",
     },
     {
         question: "What was James Potter's position in Quidditch?",
-        answer: ["Beater", "Chaser", "Keeper"],
+        answer: ["Beater", "Chaser", "Seeker", "Keeper"],
         correctAnswer: "Seeker",
-        image: "",
+        image: "quidditch.jpg",
     },
     {
         question: "Which of these is a type of Love Potion?",
-        answer: ["Felix Felicis", "Polyjuice Potion", "Veritaserum"],
+        answer: ["Amortentia", "Felix Felicis", "Polyjuice Potion", "Veritaserum"],
         correctAnswer: "Amortentia",
-        image: "",
+        image: "potions.jpg",
     },
     {
         question: "What class did Neville end up teaching at Hogwarts?",
-        answer: ["Astronomy", "Muggle Studies", "Charms"],
+        answer: ["Astronomy", "Herbology", "Muggle Studies", "Charms"],
         correctAnswer: "Herbology",
-        image: "",
+        image: "neville.jpg",
     },
     {
         question: "Which of these are not one of Hagrid's many pets?",
-        answer: ["Fluffy", "Norberta", "Aragog"],
+        answer: ["Fluffy", "Norberta", "Grawp", "Aragog"],
         correctAnswer: "Grawp",
-        image: "",
+        image: "hagrid.jpg",
     },
     {
         question: "Who was the original bearer of the cloak of invisibility?",
-        answer: ["Harry Potter", "James Potter", "Albus Dumbledore"],
+        answer: ["Harry Potter", "James Potter", "Albus Dumbledore", "Ignotus Peverell"],
         correctAnswer: "Ignotus Peverell",
-        image: "",
+        image: "cloak.jpg",
     },
     {
         question: "Which of these was not a horcrux of Lord Voldemort?",
-        answer: ["Nagini", "The diadem of Rowena Ravenclaw", "Tom Riddle's Diary"],
+        answer: ["Nagini", "The diadem of Rowena Ravenclaw", "The sword of Godric Gryffindor", "Tom Riddle's Diary"],
         correctAnswer: "The sword of Godric Gryffindor",
-        image: "",
+        image: "voldemort.jpg",
     },
     {
         question: "What was Dumbledore's sisters name?",
-        answer: ["Rita", "Minerva", "Abella"],
+        answer: ["Ariana", "Rita", "Minerva", "Abella"],
         correctAnswer: "Ariana",
-        image: "",
+        image: "ariana-dumbledore.jpg",
     },
     {
         question: "Finish This Quote: 'I open at the...'",
-        answer: ["dawn", "light", "end"],
+        answer: ["dawn", "close", "light", "end"],
         correctAnswer: "close",
-        image: "",
+        image: "snitch.jpg",
     },
     {
         question: "After all this time?",
-        answer: ["Yes", "It never stops", "I still care"],
+        answer: ["Yes", "It never stops", "Always", "I still care"],
         correctAnswer: "Always",
-        image: "",
+        image: "always.jpg",
     },
 ];
 
@@ -68,27 +68,33 @@ var game = {
     correct: 0,
     incorrect: 0,
     currentQuestion: 0,
+    answerClicked: false,
     countdown: function () {
-        intervalId = setInterval( function() {
+        intervalId = setInterval(function () {
             counter--;
             $("#counter-number").text(counter);
-            if(counter <= 0) {
+            if (counter <= 0) {
                 clearInterval(intervalId);
-                timeUp();
+                game.timeUp();
             }
         }, 1000);
     },
     loadQuestion: function () {
-        for(var i = 0; i < questions.length; i++) {
-            $(".questionArea").append(questions.question[i]);
-            var answer1 = $("<button>").text(questions.question[i].answer[0]);
-            var answer2 = $("<button>").text(questions.question[i].answer[1]);
-            var answer3 = $("<button>").text(questions.question[i].answer[2]);
-            var answer4 = $("<button>").text(questions.question[i].correctAnswer);
-            $(".questionArea").append(answer1, answer2, answer3, answer4);
-        }
 
+        game.answerClicked = false;
 
+        $(".questionArea").empty();
+
+        $(".questionArea").append(questions[game.currentQuestion].question).addClass("lead p-3 text-center");
+        var answer1 = $("<button>").text(questions[game.currentQuestion].answer[0]).addClass("btn btn-outline-dark btn-block");
+        var answer2 = $("<button>").text(questions[game.currentQuestion].answer[1]).addClass("btn btn-outline-dark btn-block");
+        var answer3 = $("<button>").text(questions[game.currentQuestion].answer[2]).addClass("btn btn-outline-dark btn-block");
+        var answer4 = $("<button>").text(questions[game.currentQuestion].answer[3]).addClass("btn btn-outline-dark btn-block");
+        $(".questionArea").append(answer1, answer2, answer3, answer4);
+
+        $(".questionArea button").on("click", game.clicked)
+
+        $(".question-image").attr("src", "assets/images/" + questions[game.currentQuestion].image);
 
         //dynamically add question into card, card.html("<h2>"+ +"</h2>")
         //for loop
@@ -96,38 +102,105 @@ var game = {
         //dynamically added answer buttons
     },
     nextQuestion: function () {
+        game.currentQuestion++;
         counter = 30;
         game.countdown();
+        game.loadQuestion();
         //use jquery to  change the text in the game counter
         //increment currentQuestion by one
         //call loadQuestion function
     },
     timeUp: function () {
-        
+    
+        $("#counter-number").text("Your time is up!");
+
+        if (game.currentQuestion === questions.length - 1) {
+            game.result();
+        } else {
+            setTimeout(game.nextQuestion, 1000);
+        }
+
         //change the text in game counter
         //append correct answer to the card
         //append image
     },
     result: function () {
+        clearInterval(intervalId);
+
+        $(".questionArea").empty();
+        $(".questionArea").removeClass("p-3");
+        
+        var results = $("<h2>").text("You had " + game.correct + " correct and " + game.incorrect + " incorrect answers.");
+
+        $(".question-image").attr("src", "");
+        $(".timer").empty();
+        $(".timer").append(results);
+
         //clearInterval
         //dynamically change html to view the results
         //append correct answer to the card
         //append image
     },
-    clicked: function () {
+    clicked: function (ev) {
+        clearInterval(intervalId);
+
+        if (!game.answerClicked) {
+
+            game.answerClicked = true;
+
+            var correctAnswer = questions[game.currentQuestion].correctAnswer;
+            var clickedAnswer = ev.target.innerText;
+
+            if (correctAnswer === clickedAnswer) {
+                game.answeredCorrectly(ev.target);
+            } else {
+                game.answeredIncorrectly(ev.target);
+            }
+
+            if (game.currentQuestion === questions.length - 1) {
+                game.result();
+            } else {
+                setTimeout(game.nextQuestion, 3000);
+            }
+
+        }
         //clearInterval
         //if/else statement for an answer clicked
         //if correct, run answeredCorrectly() function
         //else, run answeredIncorrectly() function
     },
-    answeredCorrectly: function () {
+    answeredCorrectly: function (elem) {
+        game.correct++;
+        clearInterval(intervalId);
+
+        $(".question-image").attr("src", "assets/images/snape.gif");
+
+        elem.classList.add("btn-success")
+        elem.classList.remove("btn-outline-dark");
         //correct++
         //clearInterval
         //dynamically add html to show that the answer is correct
         //add image
         //if no more questions left, show result, else wait for 3 seconds and show the next question
     },
-    answeredIncorrectly: function () {
+    answeredIncorrectly: function (elem) {
+        game.incorrect++;
+        clearInterval(intervalId);
+        elem.classList.add("btn-danger");
+        elem.classList.remove("btn-outline-dark");
+
+        $(".question-image").attr("src", "assets/images/giphy.gif");
+
+        setTimeout(function () {
+            var correctAnswer = questions[game.currentQuestion].correctAnswer;
+            $(".questionArea button").each(function (index, el) {
+                if (el.innerText === correctAnswer) {
+                    el.classList.add("btn-success")
+                    el.classList.remove("btn-outline-dark");
+                }
+            });
+        }, 500);
+
         //incorrect++
         //clearInterval
         //dynamically add html to show that the answer is incorrect
@@ -136,7 +209,9 @@ var game = {
         //if no more questions left, show result, else wait for 3 seconds and show the next question
     },
     reset: function () {
-
+        game.correct = 0;
+        game.incorrect = 0;
+        game.currentQuestion = 0;
     }
 }
 
@@ -148,13 +223,9 @@ $(".startButton").on("click", function () {
         game.countdown();
         game.loadQuestion();
     }
-})
-
-$(".answerButton").on("click", function () {
-
-})
+});
 
 $(".startOverButton").on("click", function () {
     isTimerDisplayed = false;
 
-})
+});
